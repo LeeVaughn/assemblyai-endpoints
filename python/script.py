3e0ebad89b7883e599a55854479cd69c444fcc34
LeeVaughn <dlvprogramming@gmail.com>
2021-11-02 12:51:16 -0700

    Add function and route to submit local files

--- python/script.py
+++ python/script.py
@@ -21,3 +21,19 @@ pprint(response.json())
 # request a single transcript and print the response
 response = requests.get(endpoint, headers=headers)
 pprint(response.json())
+
+# reads file and returns data
+def read_file(filename, chunk_size=5242880):
+    with open(filename, 'rb') as _file:
+        while True:
+            data = _file.read(chunk_size)
+            if not data:
+                break
+            yield data
+ 
+# uploads a local file and gets back an audio_url
+headers2 = {'authorization': "b62141f4d92b43f8a0f018ae6c8e018c"}
+response = requests.post('https://api.assemblyai.com/v2/upload',
+                         headers=headers2,
+                         data=read_file("../audio/convo.MP3"))
+pprint(response.json())
